bc1a612f5f673d9318f8a674faeae818aea866c5
Yu <yukunchan1233@gmail.com>
2025-08-19 09:11:35 +0900

    practice

--- src/basic/Practice3.java
+++ src/basic/Practice3.java
@@ -8,8 +8,46 @@ public class Practice3 {
 
 		Scanner sc = new Scanner(System.in);
 
-		int score = sc.nextInt();
-		int year = sc.nextInt();
+		int x, y;
+		String cal;
+		int result;
+
+		System.out.print("정수 입력 : ");
+		x = sc.nextInt();
+		System.out.print("정수 입력 : ");
+		y = sc.nextInt();
+		System.out.println("계산기 기호선택(+,-,*,/,)");
+		cal = sc.next();
+		result = 0;
+		String presult = "";
+
+		switch (cal) {
+		case "+":
+			result = x + y;
+			presult = "덧셈 결과"; // presult = "덧셈결과 :"+result -----문자열+정수 = 문자열
+			break;
+		// Java는 + 연산자에서 피연산자 중 하나라도 문자열이면, 문자열 결합 연산으로 동작합니다.
+		// 그래서 presult + result는 "덧셈 결과" + 5 → "덧셈 결과5"가 됩니다.
+		case "-":
+			result = x - y;
+			presult = "뺄셈 결과";
+			break;
+		case "*":
+			result = x * y;
+			presult = "곱셈 결과";
+			break;
+		case "/":
+			result = x / y;
+			presult = "나눗셈 결과";
+			break;
+		case "%":
+			result = x % y;
+			presult = "나머지 결과";
+			break;
+		default:
+			System.out.println("기호선택을 다시하세요");
+		}
+		System.out.println(presult + result); // sysout(presult)
 
 	}
 
